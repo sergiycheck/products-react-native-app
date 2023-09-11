1b0d0f31fd8aa642d5f6b7b49c21b5f483115892
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-09-11 21:28:20 +0300

    feat: add icons

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
 function HomeScreen() {
   return (
     <View style={styles.container}>
@@ -24,7 +26,23 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = "";
+
+          if (route.name === "Home") {
+            iconName = focused ? "home-circle" : "home-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "account-settings" : "account-settings-outline";
+          }
+
+          // You can return any component that you like here!
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
