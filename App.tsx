0db1ca58c09ded7aa6fdf52bb3ca7023c59e04f4
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-09-11 23:21:55 +0300

    feat: add async storage persistor

--- App.tsx
+++ App.tsx
@@ -2,9 +2,12 @@ import {AddProduct} from "./src/features/add-product/add-product";
 import {Products} from "./src/features/products/products";
 import {ScreenNames} from "./src/features/screen-names";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {createAsyncStoragePersister} from "@tanstack/query-async-storage-persister";
+import {QueryClient} from "@tanstack/react-query";
+import {PersistQueryClientProvider} from "@tanstack/react-query-persist-client";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 
 const Stack = createNativeStackNavigator();
@@ -18,15 +21,25 @@ function MyTabs() {
   );
 }
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+const asyncStoragePersister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+});
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <QueryClientProvider client={queryClient}>
+        <PersistQueryClientProvider client={queryClient} persistOptions={{persister: asyncStoragePersister}}>
           <MyTabs />
-        </QueryClientProvider>
+        </PersistQueryClientProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
